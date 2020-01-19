27ba08b5c8a53576c1d1d36e8ca5c75340930932
CraigA1987 <48647012+CraigA1987@users.noreply.github.com>
2020-01-19 10:04:53 +0000

    Update main.js

--- main.js
+++ main.js
@@ -70,7 +70,10 @@ xwingLoader.load(
 
 );
 
-document.getElementsByTagName("BODY")[0].onload = function(){console.log('All Three.js Objects loaded');};
+document.getElementsByTagName("BODY")[0].onload = function(){console.log('All Three.js Objects loaded');
+							    document.getElementsByTagName("h1").display = "block";
+							    document.getElementsByTagName("h2").display = "block";
+							    document.getElementsById("rebel").display = "block";};
 
 const yodaLoader = new THREE.GLTFLoader(); // loads in the loader file
 yodaLoader.load("yoda/scene.gltf", gltf => {
